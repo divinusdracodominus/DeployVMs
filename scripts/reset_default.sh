3b4a9d4d7bd40d13178d0960c11c16bbfd17d921
Julian Lazaras <divinusdracodominus@gmail.com>
2025-04-21 23:00:42 -0400

    going to try a depoyment

--- scripts/reset_default.sh
+++
@@ -1,5 +0,0 @@
-virsh net-destroy default
-virsh net-undefine default
-virsh net-define ./default.xml
-virsh net-start default
-virsh net-autostart default
--- scripts/rpgenkey.sh
+++
@@ -1,7 +0,0 @@
-file=$1
-if [[ $file -eq "" ]]; then
-    echo "please enter a file root"
-    exit -1
-fi
-rp genkey rosenpass-secret
-rp pubkey rosenpass-secret rosenpass-public
